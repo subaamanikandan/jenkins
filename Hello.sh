c6697f5b896b97e9da755d3a486cc7d2e229e919
subaamanikandan <subaamanikandan@gmail.com>
2024-10-03 16:06:37 +0530

    Create Hello.sh

---
+++ Hello.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Hello, World!"
+git init
+git add hello_world.sh
+git commit -m "Add hello world script"
+https://github.com/subaamanikandan/my-jenkins-project.git
+git push -u origin master
+git add .
+git commit -m "Testing Jenkins build trigger"
+git push origin master
